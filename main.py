414728c537d0fe14e9ab41353793713ea9ca96a4
Alexis Deviney <70820575+adeviney@users.noreply.github.com>
2022-11-26 19:15:40 -0800

    added lambda_function_s3.py as separate from the driver main.py. the lambda_function_s3.py reflects the actual lambda function on AWS, and main uses a mock response from API Gateway to test it

--- main.py
+++ main.py
@@ -1,45 +1,12 @@
-import boto3
+from lambda_function_s3 import *
 import hashlib
 
+shaHash = hashlib.sha1(b'a').hexdigest()
+endpoint_request_body = {
+    "shaHash": f"{shaHash}",
+    "context": {}
+}
 
 
-
-def getPasswordByQueryS3(shaHash):
-    s3 = boto3.client('s3')
-
-    BUCKET_NAME = 'crackstation-data'
-    FILE_NAME = 'hashdata.csv.gz'
-
-    resp = s3.select_object_content(
-        Bucket=BUCKET_NAME,
-        Key=FILE_NAME,
-        ExpressionType='SQL',
-        Expression=f"SELECT password FROM S3object s where s.shaHash = '{shaHash}'",
-        InputSerialization = {'CSV': {"FileHeaderInfo": "Use"}, 'CompressionType': 'GZIP'},
-        OutputSerialization = {'CSV': {}},
-    )
-
-    for event in resp['Payload']:
-        if 'Records' in event:
-            records = event['Records']['Payload'].decode('utf-8')
-            return records
-        elif 'Stats' in event:
-            statsDetails = event['Stats']['Details']
-            print("Stats details bytesScanned: ")
-            print(statsDetails['BytesScanned'])
-            print("Stats details bytesProcessed: ")
-            print(statsDetails['BytesProcessed'])
-            print("Stats details bytesReturned: ")
-            print(statsDetails['BytesReturned'])
-
-hashedPassword = hashlib.sha1(b"a").hexdigest()
-crackedPassword = getPasswordByQueryS3(hashedPassword)
-print(crackedPassword)
-
-
-def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+response_from_lambda = lambda_handler(event = endpoint_request_body, context = endpoint_request_body['context'])
+print(response_from_lambda)
