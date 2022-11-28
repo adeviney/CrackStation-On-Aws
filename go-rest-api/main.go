a495e6d9feab8907bb514c4533e99ab3ef37e288
Alexis Deviney <70820575+adeviney@users.noreply.github.com>
2022-11-27 17:36:46 -0800

    removed mock collection of passwords, and wrote the POST request handler to conform to PRD. checks whether the body has valid JSON, if so, it gets the password from dynamodb

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -25,39 +25,43 @@ type crackstationAppError struct {
 	Code    int
 }
 
-// handful of passwords to mock
-var passwords = []password{
-	{ShaHash: "86f7e437faa5a7fce15d1ddcb9eaeaea377667b8", Password: "a"},
-	{ShaHash: "e9d71f5ee7c92d6dc9e92ffdad17b8bd49418f98", Password: "b"},
-	{ShaHash: "84a516841ba77a5b4648de2cd0dfcb30ea46dbb4", Password: "c"},
+type PostRequest struct {
+	ShaHash string `json:"sha_hash"`
 }
 
 func postPasswords(c *gin.Context) {
-	var newPassword password
+	var shaHashRequest PostRequest
 
-	if err := c.BindJSON(&newPassword); err != nil {
+	if err := c.BindJSON(&shaHashRequest); err != nil || shaHashRequest.ShaHash == "" {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "invalid JSON or no sha_hash present. See documentation."})
 		return
 	}
 
-	passwords = append(passwords, newPassword)
-	c.IndentedJSON(http.StatusCreated, newPassword)
+	pwAsStruct, err := getPasswordByHash(shaHashRequest.ShaHash)
+	if err != nil { // err is *crackstationAppError, not os.Error
+		c.IndentedJSON(err.Code, gin.H{"requestedShaHash": shaHashRequest.ShaHash, "message": err.Message})
+		return
+	} else {
+		pwAsJSON, _ := json.Marshal(pwAsStruct)
+		c.Data(http.StatusOK, "application/json", pwAsJSON)
+	}
 }
 
 func getPassword(c *gin.Context) {
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Println("Recovered in f", r)
-			c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "unexpected error"})
+			c.JSON(http.StatusInternalServerError, gin.H{"message": "unexpected error"})
 		}
 	}()
 	hash := c.Param("shahash")
 	if hash == "" {
-		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "missing shahash parameter"})
+		c.JSON(http.StatusBadRequest, gin.H{"message": "missing shahash parameter"})
 		return
 	} else {
 		pwAsStruct, err := getPasswordByHash(hash)
 		if err != nil { // err is *crackstationAppError, not os.Error
-			c.IndentedJSON(err.Code, gin.H{"requestedShaHash": hash, "message": err.Message})
+			c.JSON(err.Code, gin.H{"requestedShaHash": hash, "message": err.Message})
 			return
 		} else {
 			pwAsJSON, _ := json.Marshal(pwAsStruct)
@@ -68,7 +72,11 @@ func getPassword(c *gin.Context) {
 }
 
 func getPasswordByHash(hash string) (*password, *crackstationAppError) {
-	var db = dynamodb.New(session.New(), aws.NewConfig().WithRegion("us-east-1"))
+	sess, err := session.NewSession()
+	if err != nil {
+		return nil, &crackstationAppError{err, "could not reach dynamoDB. GetItem failed.", 500}
+	}
+	var db = dynamodb.New(sess, aws.NewConfig().WithRegion("us-east-1"))
 
 	input := &dynamodb.GetItemInput{
 		TableName: aws.String("rainbowlookup"),
@@ -101,14 +109,14 @@ func getPasswordByHash(hash string) (*password, *crackstationAppError) {
 }
 
 func missingShaHash(c *gin.Context) {
-	c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "missing shahash parameter"})
+	c.JSON(http.StatusBadRequest, gin.H{"message": "missing shahash parameter"})
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/passwords/:shahash", getPassword)
 	router.GET("/passwords", missingShaHash)
-	router.POST("/passwords", postPasswords)
+	router.POST("/decrypt", postPasswords)
 
 	router.Run("localhost:8080")
 }
