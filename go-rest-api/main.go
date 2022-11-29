72c1827d9dd28336246f0eb865d61fecc2b54dba
Alexis Deviney <70820575+adeviney@users.noreply.github.com>
2022-11-29 11:10:03 -0800

    added memcache cache

--- go-rest-api/main.go
+++ go-rest-api/main.go
@@ -9,10 +9,13 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/bradfitz/gomemcache/memcache"
 	"github.com/gin-gonic/gin"
 )
 
 // adapted from tutorial https://go.dev/doc/tutorial/web-service-gin
+
+var mc = memcache.New("passwordcache.4fxm87.cfg.use1.cache.amazonaws.com:11211")
 
 type password struct {
 	ShaHash  string `json:"shaHash"`
@@ -55,20 +58,56 @@ func getPassword(c *gin.Context) {
 		}
 	}()
 	hash := c.Param("shahash")
+
 	if hash == "" {
 		c.JSON(http.StatusBadRequest, gin.H{"message": "missing shahash parameter"})
 		return
 	} else {
-		pwAsStruct, err := getPasswordByHash(hash)
-		if err != nil { // err is *crackstationAppError, not os.Error
-			c.JSON(err.Code, gin.H{"requestedShaHash": hash, "message": err.Message})
-			return
+		pwFromCache, err := checkCache(hash)
+		if err != nil { // not cached
+			pwAsStruct, err := getPasswordByHash(hash)
+			if err != nil { // err is *crackstationAppError, not os.Error
+				c.JSON(err.Code, gin.H{"requestedShaHash": hash, "message": err.Message})
+				return
+			} else {
+				pwAsJSON, _ := json.Marshal(pwAsStruct)
+				fmt.Printf("pwAsJson %T\n", pwAsJSON)
+				fmt.Println(pwAsJSON)
+				cacheErr := mc.Set(&memcache.Item{Key: hash, Value: []byte(pwAsStruct.Password), Expiration: 10})
+				if cacheErr != nil {
+					fmt.Printf("there was an error adding to cache")
+					print(cacheErr)
+				}
+				c.Data(http.StatusOK, "application/json", pwAsJSON)
+				return
+			}
 		} else {
-			pwAsJSON, _ := json.Marshal(pwAsStruct)
+			fmt.Printf("pwFromCache %T\n", pwFromCache)
+			pwAsJSON, _ := json.Marshal(pwFromCache)
+			fmt.Println(pwAsJSON)
 			c.Data(http.StatusOK, "application/json", pwAsJSON)
+			return
 		}
+
 	}
 
+}
+
+func checkCache(hash string) (*memcache.Item, error) {
+	val, err := mc.Get(hash)
+	if err != nil {
+		return nil, err
+	}
+
+	//pw := password{}
+
+	// err = json.Unmarshal(val, &pw)
+
+	// if err != nil {
+	// 	panic(fmt.Sprintf("Failed to UnmarshalMap result.Item: %s", err.Error()))
+	// }
+
+	return val, nil
 }
 
 func getPasswordByHash(hash string) (*password, *crackstationAppError) {
